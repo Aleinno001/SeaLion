f0f98b4ea4cd935f1478ca900d341dae015262b5
davide <davide.lombardi2@stud.unifi.it>
2021-07-02 18:59:15 +0200

    Definizione header View + getter

---
+++ View.cpp
@@ -0,0 +1,13 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#include "View.h"
+
+WarShip *View::getModel() const {
+    return model;
+}
+
+Bonus *View::getController() const {
+    return controller;
+}
---
+++ View.h
@@ -0,0 +1,29 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#ifndef SEALION_VIEW_H
+#define SEALION_VIEW_H
+
+#include "Bonus.h"
+
+
+class View {
+
+private:
+    WarShip *model;
+    Bonus *controller;
+public:
+    View(WarShip *m, Bonus *c) : model(m), controller(c) {};
+
+    WarShip *getModel() const;
+
+    Bonus *getController() const;
+
+    void update();
+
+    void bonusButtonPress();
+};
+
+
+#endif //SEALION_VIEW_H
