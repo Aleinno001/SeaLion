34a770fbf17bebcd4e787ab79343135f902f6edf
InnocentiA <alessandro.innocenti8@stud.unifi.it>
2021-07-02 17:37:58 +0200

    completato header Arsenal + definizione getter

--- Arsenal.cpp
+++ Arsenal.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "Arsenal.h"
+
+//getter
+const float Arsenal::getRangeOfFire() const {
+    return rangeOfFire;
+}
+
+const int Arsenal::getReloadTime() const {
+    return reloadTime;
+}
+
+const int Arsenal::getRateOfFire() const {
+    return rateOfFire;
+}
+
+int Arsenal::getAmmoSpeed() const {
+    return ammoSpeed;
+}
+
+int Arsenal::getMaximumDispersion() const {
+    return maximumDispersion;
+}
+
+const std::string &Arsenal::getAmmoType() const {
+    return ammoType;
+}
+
+float Arsenal::getAmmoDeceleration() const {
+    return ammoDeceleration;
+}
+
+int Arsenal::getFirepower() const {
+    return firepower;
+}
+
+int Arsenal::getNumAmmo() const {
+    return numAmmo;
+}
+
+double Arsenal::getPosX() const {
+    return posX;
+}
+
+double Arsenal::getPosY() const {
+    return posY;
+}
--- Arsenal.h
+++ Arsenal.h
@@ -5,10 +5,60 @@
 #ifndef SEALION_ARSENAL_H
 #define SEALION_ARSENAL_H
 
+#include <cstdlib>
+#include <string>
+#include "Vehicle.h"
 
 class Arsenal {
+protected:
+    const float rangeOfFire;
+    const int reloadTime;
+    const int rateOfFire;
+    int ammoSpeed;
+    int maximumDispersion;
+    std::string ammoType;
+    float ammoDeceleration;
+    int firepower;
+    int numAmmo;
+    double posX;
+    double posY;  //FIXME posX,posY da rivedere quando la mappa sarà definita
+    //TODO sprite/grafica
+public:
+    //TODO implementare i metodi
+    virtual void openFire(Vehicle enemy);
 
+    virtual void update(double posX, double posY); //FIXME posX,posY da rivedere
+    virtual void attach();
+
+    virtual void detach();
+
+    //getter
+    const float getRangeOfFire() const;
+
+    const int getReloadTime() const;
+
+    const int getRateOfFire() const;
+
+    int getAmmoSpeed() const;
+
+    int getMaximumDispersion() const;
+
+    const std::string &getAmmoType() const;
+
+    float getAmmoDeceleration() const;
+
+    int getFirepower() const;
+
+    int getNumAmmo() const;
+
+    double getPosX() const;
+
+    double getPosY() const;
+
+protected:
+    virtual bool engage(Vehicle enemy) const = 0;
+
+    virtual void rotate() const = 0;
 };
-
 
 #endif //SEALION_ARSENAL_H
