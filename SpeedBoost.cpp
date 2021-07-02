39812a77a2c5dc593da723cef919622d1ffe41a4
davide <davide.lombardi2@stud.unifi.it>
2021-07-02 18:40:36 +0200

    Definizione header SpeedBoost + getter

---
+++ SpeedBoost.cpp
@@ -0,0 +1,17 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#include "SpeedBoost.h"
+
+int SpeedBoost::getUses() const {
+    return uses;
+}
+
+float SpeedBoost::getDuration() const {
+    return duration;
+}
+
+float SpeedBoost::getMultiplier() const {
+    return multiplier;
+}
---
+++ SpeedBoost.h
@@ -0,0 +1,33 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#ifndef SEALION_SPEEDBOOST_H
+#define SEALION_SPEEDBOOST_H
+
+#include "Bonus.h"
+
+class SpeedBoost : public Bonus {
+
+private:
+    int uses;
+    float duration;
+    float multiplier;
+
+public:
+    SpeedBoost(WarShip *m, float c, int u, float d, float mult) : Bonus(m, c), uses(u), duration(d),
+                                                                  multiplier(mult) {};
+
+    ~SpeedBoost() override;
+
+    int getUses() const;
+
+    float getDuration() const;
+
+    float getMultiplier() const;
+
+    void activate() override;
+};
+
+
+#endif //SEALION_SPEEDBOOST_H
