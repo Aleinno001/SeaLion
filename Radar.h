65bd2b82ec4b9d697f14d097c19f4a6935794455
davide <davide.lombardi2@stud.unifi.it>
2021-07-02 17:56:12 +0200

    Definizione header Radar

---
+++ Radar.h
@@ -0,0 +1,24 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#ifndef SEALION_RADAR_H
+#define SEALION_RADAR_H
+
+#include "Bonus.h"
+
+class Radar : public Bonus {
+
+private:
+    float radius;
+    float duration;
+public:
+    Radar(WarShip *model, float c, float r, float d) : Bonus(model, c), radius(r), duration(d) {};
+
+    ~Radar() override;
+
+    void activate() override;
+};
+
+
+#endif //SEALION_RADAR_H
