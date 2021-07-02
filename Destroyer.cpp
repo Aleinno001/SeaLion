665415c7994225f0ca788ad36ec5dd9768130255
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-02 09:53:23 -0700

    Aggiunta header di Destroyer

--- Destroyer.cpp
+++ Destroyer.cpp
@@ -1,5 +1,70 @@
 //
-// Created by alessandro on 02/07/21.
+// Created by pala on 7/2/21.
 //
 
 #include "Destroyer.h"
+
+float Destroyer::calcSpeed(Map battleSea) {
+    return Vehicle::calcSpeed(battleSea);
+}
+
+void Destroyer::attack(Vehicle target) {
+    Vehicle::attack(target);
+}
+
+void Destroyer::calculateDistance(Vehicle target) {
+    Vehicle::calculateDistance(target);
+}
+
+void Destroyer::update(bool isDead) {
+    Vehicle::update(isDead);
+}
+
+void Destroyer::attach() {
+    Vehicle::attach();
+}
+
+void Destroyer::detach() {
+    Vehicle::detach();
+}
+
+void Destroyer::stopMove() {
+
+}
+
+void Destroyer::ceaseFire() {
+
+}
+
+void Destroyer::registerArsenals() {
+
+}
+
+void Destroyer::removeArsenals() {
+
+}
+
+void Destroyer::notifyArsenals() {
+
+}
+
+Destroyer::~Destroyer() {
+
+}
+
+void Destroyer::rotate() {
+    Vehicle::rotate();
+}
+
+bool Destroyer::canEngage(Vehicle target) {
+    return Vehicle::canEngage(target);
+}
+
+Destroyer::Destroyer(double x, double y, float ac, const float maxVel, int hp, int length, int arm, std::string na,
+                     std::string nat, int numL, int numH, int numM,
+                     std::vector<std::unique_ptr<Arsenal>> &arsenalList,
+                     std::vector<std::unique_ptr<Vehicle>> &vehicleList) : WarShip(x, y, ac, maxVel, hp, length, arm,
+                                                                                   na, nat, numL, numH, numM,
+                                                                                   arsenalList, vehicleList) {
+
+}
--- Destroyer.h
+++ Destroyer.h
@@ -1,13 +1,51 @@
 //
-// Created by alessandro on 02/07/21.
+// Created by pala on 7/2/21.
 //
 
 #ifndef SEALION_DESTROYER_H
 #define SEALION_DESTROYER_H
 
 
-class Destroyer {
+#include "WarShip.h"
 
+class Destroyer : public WarShip {
+private:
+    int numTorpedoTubes;
+public:
+
+    Destroyer(double x, double y, float ac, const float maxVel, int hp, int length, int arm, std::string na,
+              std::string nat, int numL, int numH, int numM,
+              std::vector<std::unique_ptr<Arsenal>> &arsenalList,
+              std::vector<std::unique_ptr<Vehicle>> &vehicleList);
+
+    float calcSpeed(Map battleSea) override;
+
+    void attack(Vehicle target) override;
+
+    void calculateDistance(Vehicle target) override;
+
+    void update(bool isDead) override;
+
+    void attach() override;
+
+    void detach() override;
+
+    void stopMove() override;
+
+    void ceaseFire() override;
+
+    void registerArsenals() override;
+
+    void removeArsenals() override;
+
+    void notifyArsenals() override;
+
+    ~Destroyer() override;
+
+protected:
+    void rotate() override;
+
+    bool canEngage(Vehicle target) override;
 };
 
 
