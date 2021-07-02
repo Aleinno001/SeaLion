de0a7b5b7dd7fc6395d4c3775657c5e06d1fbb51
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-02 08:39:43 -0700

    Scrittura dell'header della classe WarShip

--- WarShip.cpp
+++ WarShip.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "WarShip.h"
+
+const int WarShip::getArmour() const {
+    return armour;
+}
+
+const std::string &WarShip::getName() const {
+    return name;
+}
+
+const std::string &WarShip::getNationality() const {
+    return nationality;
+}
+
+const int WarShip::getNumLCannons() const {
+    return numLCannons;
+}
+
+const int WarShip::getNumMCannons() const {
+    return numMCannons;
+}
+
+const int WarShip::getNumHCannons() const {
+    return numHCannons;
+}
+
+const std::vector<std::unique_ptr<Arsenal>> &WarShip::getArsenalList() const {
+    return arsenalList;
+}
+
+const std::vector<std::unique_ptr<Vehicle>> &WarShip::getVehicleList() const {
+    return vehicleList;
+}
+
+WarShip::WarShip(int arm, std::string na, std::string nat, int numL, int numH, int numM,
+                 std::vector<std::unique_ptr<Arsenal>> arsenalList, std::vector<std::unique_ptr<Vehicle>> vehicleList)
+        : armour(arm), name(na), nationality(nat), numLCannons(numL), numHCannons(numH), numMCannons(numM) {
+
+}
+
--- WarShip.h
+++ WarShip.h
@@ -5,11 +5,53 @@
 #ifndef SEALION_WARSHIP_H
 #define SEALION_WARSHIP_H
 
+#include <string>
+#include <memory>
+#include <vector>
 #include "Vehicle.h"
+#include "Arsenal.h"
 
 class WarShip : public Vehicle {
+protected:
+    const int armour;
+    const std::string name;
+    const std::string nationality;
+    const int numLCannons;
+    const int numMCannons;
+    const int numHCannons;
+    std::vector<std::unique_ptr<Arsenal>> arsenalList;
+    std::vector<std::unique_ptr<Vehicle>> vehicleList;
+public:
+    WarShip(int arm, std::string na, std::string nat, int numL, int numH, int numM,
+            std::vector<std::unique_ptr<Arsenal>> arsenalList, std::vector<std::unique_ptr<Vehicle>> vehicleList);
 
+    virtual void stopMove() = 0;
 
+    virtual void ceaseFire() = 0;
+
+    virtual void registerArsenals() = 0;
+
+    virtual void removeArsenals() = 0;
+
+    virtual void notifyArsenals() = 0;
+
+    const int getArmour() const;
+
+    const std::string &getName() const;
+
+    const std::string &getNationality() const;
+
+    const int getNumLCannons() const;
+
+    const int getNumMCannons() const;
+
+    const int getNumHCannons() const;
+
+    const std::vector<std::unique_ptr<Arsenal>> &getArsenalList() const;
+
+    const std::vector<std::unique_ptr<Vehicle>> &getVehicleList() const;
+
+    virtual ~WarShip() = 0;
 };
 
 
