40de6d30fc92342c9112667352ecb23e8292f827
davide <davide.lombardi2@stud.unifi.it>
2021-07-02 18:27:47 +0200

    Definizione header FireExtinguisher + getter

---
+++ FireExtinguisher.cpp
@@ -0,0 +1,9 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#include "FireExtinguisher.h"
+
+int FireExtinguisher::getUses() const {
+    return uses;
+}
---
+++ FireExtinguisher.h
@@ -0,0 +1,27 @@
+//
+// Created by davide on 02/07/21.
+//
+
+#ifndef SEALION_FIREEXTINGUISHER_H
+#define SEALION_FIREEXTINGUISHER_H
+
+#include "Bonus.h"
+
+class FireExtinguisher : public Bonus {
+
+private:
+    int uses;
+
+public:
+    FireExtinguisher(WarShip *m, float c, int u) : Bonus(m, c), uses(u) {};
+
+    ~FireExtinguisher() override;
+
+    int getUses() const;
+
+    void activate() override;
+
+};
+
+
+#endif //SEALION_FIREEXTINGUISHER_H
