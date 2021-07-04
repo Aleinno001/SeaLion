0088bce0c6090537b445b49b0f28140b001815e9
InnocentiA <alessandro.innocenti8@stud.unifi.it>
2021-07-04 10:01:23 +0200

    aggiunta classe Fighter

---
+++ Fighter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by alessandro on 04/07/21.
+//
+
+#include "Fighter.h"
---
+++ Fighter.h
@@ -0,0 +1,31 @@
+//
+// Created by alessandro on 04/07/21.
+//
+
+#ifndef SEALION_FIGHTER_H
+#define SEALION_FIGHTER_H
+
+#include "Vehicle.h"
+
+class Fighter : public Vehicle {
+private:
+    Fighter(double X, double Y, float ac, const float maxVel, int HP, int length/*SpriteSheet sprite*/);
+
+    void fight(Vehicle enemy);
+
+    float calcSpeed(Map battleSea);
+
+    void attack(Vehicle target) override;
+
+    void calculateDistance(Vehicle target) override;
+
+    void update(bool isDead) override;
+
+    void attach() override;
+
+    void detach() override;
+
+    ~Fighter();
+};
+
+#endif //SEALION_FIGHTER_H
