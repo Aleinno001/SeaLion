67def20723574eeb6b7b0a7fcb372304439929cf
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-08 02:10:43 -0700

    XIII° Fase di sistemazione codice classe torpedo bomber allo scopo di implementarne le factory

--- Fighter.h
+++ Fighter.h
@@ -11,7 +11,7 @@ class Fighter : public Vehicle {
 private:
     Fighter(int x, int y, float ac1, float maxVel1, int hp, int length1, sf::Texture tex, sf::Sprite sp,
             int le, int wi, bool col, std::string textureName, double X, double Y, float ac,
-            const float maxVel, int HP, int length);//FIXME sprite
+            const float maxVel, int HP, int length);
 
     void fight(Vehicle enemy);
 
--- TorpedoBomber.cpp
+++ TorpedoBomber.cpp
@@ -3,3 +3,48 @@
 //
 
 #include "TorpedoBomber.h"
+
+TorpedoBomber::TorpedoBomber(int x, int y, float ac1, float maxVel1, int hp, int length1, sf::Texture tex,
+                             sf::Sprite sp, int le, int wi, bool col, std::string textureName, double X, double Y,
+                             float ac, const float maxVel, int HP, int length, int numTorpedos, int damage) : Vehicle(x,
+                                                                                                                      y,
+                                                                                                                      ac1,
+                                                                                                                      maxVel1,
+                                                                                                                      hp,
+                                                                                                                      length1,
+                                                                                                                      tex,
+                                                                                                                      sp,
+                                                                                                                      le,
+                                                                                                                      wi,
+                                                                                                                      col,
+                                                                                                                      textureName) {
+
+}
+
+void TorpedoBomber::launchTorpedo(Vehicle enemy) {
+
+}
+
+void TorpedoBomber::calculateDistance(Vehicle target) {
+    Vehicle::calculateDistance(target);
+}
+
+void TorpedoBomber::attack(Vehicle target) {
+    Vehicle::attack(target);
+}
+
+void TorpedoBomber::update(bool isDead) {
+    Vehicle::update(isDead);
+}
+
+void TorpedoBomber::attach() {
+    Vehicle::attach();
+}
+
+void TorpedoBomber::detach() {
+    Vehicle::detach();
+}
+
+TorpedoBomber::~TorpedoBomber() {
+
+}
