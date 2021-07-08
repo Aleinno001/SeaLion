046419f03231aa2154dc65849db1051e2c8b8cf2
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-08 02:36:38 -0700

    XXI° Fase di sistemazione codice classe MediumCannon allo scopo di implementarne le factory

--- MediumCannon.h
+++ MediumCannon.h
@@ -11,17 +11,20 @@ class MediumCannnon : public Arsenal {
 public:
     //TODO implementare
     //FIXME coordinate da rivedere + sprite
-    MediumCannnon(const float range, const int reload, const int rate, int speed, int dispersion, std::string type,
-                  float decelleration, int power, int num, double x, double y);
+    MediumCannnon(const float range1, const int reload1, const int rate1, int speed1, int dispersion1,
+                  std::string type1, float decelleration1, int power1, int num1, int x1, int y1,
+                  sf::Texture tex, sf::Sprite sp, int le, int wi, bool col, std::string texName,
+                  const float range, const int reload, const int rate, int speed, int dispersion,
+                  std::string type, float decelleration, int power, int num, double x, double y);
 
-    ~MediumCannnon();
+    ~MediumCannnon() override;
 
 private:
     bool rapidFire();
 
     void openFire(Vehicle enemy) override;
 
-    void update(double posX, double posY) override; //FIXME posX,posY da rivedere
+    void update(int posX, int posY) override; //FIXME posX,posY da rivedere
 
     void attach() override;
 
