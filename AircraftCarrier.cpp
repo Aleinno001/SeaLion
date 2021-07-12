0ca1de09034810a0d275f5d6dd07ed1074ccb96c
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-12 07:33:28 -0700

    Risistemati il costruttore di aircraftcarrier

--- AircraftCarrier.cpp
+++ AircraftCarrier.cpp
@@ -60,16 +60,21 @@ bool AircraftCarrier::canEngage(Vehicle target) {
     return Vehicle::canEngage(target);
 }
 
-AircraftCarrier::AircraftCarrier(double x, double y, float ac, const float maxVel, int hp, int length, int arm,
-                                 std::string na, std::string nat, int numL, int numH, int numM,
+AircraftCarrier::AircraftCarrier(int x1, int y1, float ac1, const float maxVel1, int hp1, int length1, int arm1,
+                                 std::string na1, std::string nat1, int numL1, int numH1, int numM1,
+                                 std::vector<std::unique_ptr<Arsenal>> &arsenalList1,
+                                 std::vector<std::unique_ptr<Vehicle>> &vehicleList1, sf::Texture tex1,
+                                 sf::Sprite sp1, int le1, int wi1, bool col1, std::string textureName1, ShipType sh,
+                                 ModelType mo, double x, double y, float ac, const float maxVel, int hp, int length,
+                                 int arm, std::string na, std::string nat, int numL, int numH, int numM,
                                  std::vector<std::unique_ptr<Arsenal>> &arsenalList,
                                  std::vector<std::unique_ptr<Vehicle>> &vehicleList, sf::Texture tex, sf::Sprite sp,
-                                 int le, int wi,
-                                 bool col, std::string textureName) : WarShip(x, y, ac, maxVel, hp,
-                                                                              length, arm, na, nat,
-                                                                              numL, numH, numM,
-                                                                              arsenalList,
-                                                                              vehicleList, tex, sp, le, wi, col,
-                                                                              textureName) {
+                                 int le, int wi, bool col, std::string textureName) : WarShip(x1, y1, ac1, maxVel1, hp1,
+                                                                                              length1, arm1, na1, nat1,
+                                                                                              numL1, numH1, numM1,
+                                                                                              arsenalList1,
+                                                                                              vehicleList1, tex1, sp1,
+                                                                                              le1, wi1, col1,
+                                                                                              textureName1, sh, mo) {
 
 }
--- AircraftCarrier.h
+++ AircraftCarrier.h
@@ -12,11 +12,16 @@ class AircraftCarrier : public WarShip {
 private:
     int numPlanes;
 public:
-    AircraftCarrier(double x, double y, float ac, const float maxVel, int hp, int length, int arm,
-                    std::string na, std::string nat, int numL, int numH, int numM,
+    AircraftCarrier(int x1, int y1, float ac1, const float maxVel1, int hp1, int length1, int arm1,
+                    std::string na1, std::string nat1, int numL1, int numH1, int numM1,
+                    std::vector<std::unique_ptr<Arsenal>> &arsenalList1,
+                    std::vector<std::unique_ptr<Vehicle>> &vehicleList1, sf::Texture tex1,
+                    sf::Sprite sp1, int le1, int wi1, bool col1, std::string textureName1, ShipType sh,
+                    ModelType mo, double x, double y, float ac, const float maxVel, int hp, int length,
+                    int arm, std::string na, std::string nat, int numL, int numH, int numM,
                     std::vector<std::unique_ptr<Arsenal>> &arsenalList,
-                    std::vector<std::unique_ptr<Vehicle>> &vehicleList, sf::Texture tex, sf::Sprite sp, int le, int wi,
-                    bool col, std::string textureName);
+                    std::vector<std::unique_ptr<Vehicle>> &vehicleList, sf::Texture tex, sf::Sprite sp,
+                    int le, int wi, bool col, std::string textureName);
 
     //float calcSpeed() override;
 
