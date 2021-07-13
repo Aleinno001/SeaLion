96c5adaf5fb3f75739262404bc074e5ebc660dce
Gabriele <gabriele.santanni@stud.unifi.it>
2021-07-13 06:58:38 -0700

    Sistemata la struttura principale dei metodi factory per AircraftCarrier e cruiser nemiche

--- ShipFactory.cpp
+++ ShipFactory.cpp
@@ -11,7 +11,129 @@ std::unique_ptr<WarShip> ShipFactory::createSubmarine(ModelType type, GameWorld 
     switch (type) {
 
         case ModelType::I400:
+
+            break;
+
+        case ModelType::typeb1:
+
+            break;
+
+        case ModelType::DaVinci:
+
+            break;
+
+        case ModelType::Papa:
+
+            break;
+
+        case ModelType::Triton:
+
+            break;
+
+        case ModelType::Trenchant:
+
+            break;
+
+        case ModelType::Gato:
+
+            break;
+
+        case ModelType::Narwhal:
+
+            break;      //TODO add exception
+    }
+
+}
+
+std::unique_ptr<WarShip> ShipFactory::createAircraftCarrier(ModelType type, GameWorld &map) {
+    switch (type) {
+
+        case ModelType::Hiryu:
+            break;
+
+        case ModelType::Tahio:
+
+            break;
+
+        case ModelType::GiuseppeGaribaldi:
+
+            break;
+
+        case ModelType::ArkRoyal:
+
+            break;
+
+        case ModelType::Indomitable:
+
+            break;
+
+        case ModelType::Midway:
+
+            break;    //TODO add exception
+
+
+    }
+    return std::unique_ptr<WarShip>();
+}
+
+std::unique_ptr<WarShip> ShipFactory::createCruiser(ModelType type, GameWorld &map) {
+    switch (type) {
+
+        case ModelType::Takao:
+
+
+            break;
+
+        case ModelType::IsuzuNagara:
+
+            break;
+
+        case ModelType::Ijn:
+
+            break;
+
+        case ModelType::AlbertoDiGiussano:
+
+            break;
+
+        case ModelType::Gorizia:
+
+            break;
+
+        case ModelType::Trento:
+
+            break;
+
+        case ModelType::Belfast:
+
+            break;
+
+        case ModelType::Danae:
+
+            break;
+
+        case ModelType::Tiger59:
+
+            break;
+        case ModelType::Alaska:
+
+            break;
+        case ModelType::NewOrleans:
+
+            break;
+        case ModelType::StLouis:
+
+            break;//TODO add exception
+    }
+    return std::unique_ptr<WarShip>();
+}
+
+std::unique_ptr<WarShip> ShipFactory::createBattleship(ModelType type, GameWorld &map) {
+    switch (type) {
+
+        case ModelType::I400:
             //std::unique_ptr<Submarine> i400(new Submarine()); //TODO da concludere
+
 
             break;
 
@@ -43,22 +165,45 @@ std::unique_ptr<WarShip> ShipFactory::createSubmarine(ModelType type, GameWorld 
 
             break;
     }
-
-}
-
-std::unique_ptr<WarShip> ShipFactory::createAircraftCarrier(ModelType type, GameWorld &map) {
-    return std::unique_ptr<WarShip>();
-}
-
-std::unique_ptr<WarShip> ShipFactory::createCruiser(ModelType type, GameWorld &map) {
-    return std::unique_ptr<WarShip>();
-}
-
-std::unique_ptr<WarShip> ShipFactory::createBattleship(ModelType type, GameWorld &map) {
     return std::unique_ptr<WarShip>(); //TODO implementare tutti i costruttori
 }
 
 std::unique_ptr<WarShip> ShipFactory::createDestroyer(ModelType type, GameWorld &map) {
+    switch (type) {
+
+        case ModelType::I400:
+
+
+            break;
+
+        case ModelType::typeb1:
+
+            break;
+
+        case ModelType::DaVinci:
+
+            break;
+
+        case ModelType::Papa:
+
+            break;
+
+        case ModelType::Triton:
+
+            break;
+
+        case ModelType::Trenchant:
+
+            break;
+
+        case ModelType::Gato:
+
+            break;
+
+        case ModelType::Narwhal:
+
+            break;
+    }
     return std::unique_ptr<WarShip>();
 }
 
