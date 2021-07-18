f8efe6f13ce9f5480403fc21a4fd5d28ecc9095f
InnocentiA <alessandro.innocenti8@stud.unifi.it>
2021-07-18 12:23:54 +0200

    Fix lista submarine

--- Submarine.cpp
+++ Submarine.cpp
@@ -11,8 +11,8 @@ void Submarine::submerge() {
 
 Submarine::Submarine(int x, int y, float ac, const float maxVel, int hp, int arm,
                      std::string nat, int numL, int numH, int numM, int numAA,
-                     std::vector<std::unique_ptr<Arsenal>> &arsenalList,
-                     std::vector<std::unique_ptr<Vehicle>> &vehicleList, int le, int wi,
+                     std::list<std::unique_ptr<Arsenal>> &arsenalList,
+                     std::list<std::unique_ptr<Vehicle>> &vehicleList, int le, int wi,
                      bool col, ShipType sh, ModelType mo, int numTor, bool isSub) : WarShip(x, y, ac, maxVel, hp, arm,
                                                                                             nat, numL, numH, numM,
                                                                                             numAA,
