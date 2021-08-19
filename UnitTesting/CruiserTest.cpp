2e33cd86024a65503b0cfb44d8d65ca0b1d14ec3
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-19 02:10:20 -0700

    Codice di unit testing per cotruttori di cruiser Trento e Danae

--- UnitTesting/CruiserTest.cpp
+++ UnitTesting/CruiserTest.cpp
@@ -86,47 +86,62 @@ TEST(Cruiser, Constructor) {
     ASSERT_EQ(alaska->getModelType(),ModelType::Alaska);
     ASSERT_EQ(alaska->getNumPlanes(),4);
 
+    shipWidth = 14;
+    shipHeight = 136;
+    antiAir = 7;
 
+    std::unique_ptr<Cruiser> danae(new Cruiser(1000,1000, 3, 54, 5925, 190,
+                                               "Uk", 6, 0, 1, antiAir,  v, shipHeight, shipWidth, true,
+                                               ShipType::Cruiser,
+                                               ModelType::Danae, 0));
+    
+    
+    ASSERT_EQ(danae->getPos().x,1000);
+    ASSERT_EQ(danae->getPos().y,1000);
+    ASSERT_EQ(danae->getAcceleration(),3);
+    ASSERT_EQ(danae->getMaxSpeed(),54);
+    ASSERT_EQ(danae->getHp(),5925);
+    ASSERT_EQ(danae->getArmour(),190);
+    ASSERT_EQ(danae->getNationality(),"Uk");
+    ASSERT_EQ(danae->getNumLCannons(),6);
+    ASSERT_EQ(danae->getNumHCannons(),0);
+    ASSERT_EQ(danae->getNumMCannons(),1);
+    ASSERT_EQ(danae->getNumAntiAircraft(),antiAir);
+    ASSERT_EQ(danae->getWidth(),shipWidth);
+    ASSERT_EQ(danae->getLength(),shipHeight);
+    ASSERT_EQ(danae->getCol(),true);
+    ASSERT_EQ(danae->getShipType(),ShipType::Cruiser);
+    ASSERT_EQ(danae->getModelType(),ModelType::Danae);
+    ASSERT_EQ(danae->getNumPlanes(),0);
+    
+    shipWidth = 25;
+    shipHeight = 197;
+    antiAir = 12;
+
+
+    std::unique_ptr<Cruiser> trento(new Cruiser(1000,1000, 3, 66, 13548, 120,
+                                                "Italy", 6, 0, 4, antiAir,  v, shipHeight, shipWidth, true,
+                                                ShipType::Cruiser,
+                                                ModelType::Trento, 2));
     
     
     ASSERT_EQ(saintLouis->getPos().x,1000);
     ASSERT_EQ(saintLouis->getPos().y,1000);
     ASSERT_EQ(saintLouis->getAcceleration(),3);
-    ASSERT_EQ(saintLouis->getMaxSpeed(),41);
-    ASSERT_EQ(saintLouis->getHp(),11013);
-    ASSERT_EQ(saintLouis->getArmour(),280);
-    ASSERT_EQ(saintLouis->getNationality(),"Usa");
-    ASSERT_EQ(saintLouis->getNumLCannons(),5);
-    ASSERT_EQ(saintLouis->getNumHCannons(),4);
-    ASSERT_EQ(saintLouis->getNumMCannons(),3);
+    ASSERT_EQ(saintLouis->getMaxSpeed(),66);
+    ASSERT_EQ(saintLouis->getHp(),13548);
+    ASSERT_EQ(saintLouis->getArmour(),120);
+    ASSERT_EQ(saintLouis->getNationality(),"Italy");
+    ASSERT_EQ(saintLouis->getNumLCannons(),6);
+    ASSERT_EQ(saintLouis->getNumHCannons(),0);
+    ASSERT_EQ(saintLouis->getNumMCannons(),4);
     ASSERT_EQ(saintLouis->getNumAntiAircraft(),antiAir);
     ASSERT_EQ(saintLouis->getWidth(),shipWidth);
     ASSERT_EQ(saintLouis->getLength(),shipHeight);
     ASSERT_EQ(saintLouis->getCol(),true);
     ASSERT_EQ(saintLouis->getShipType(),ShipType::Cruiser);
-    ASSERT_EQ(saintLouis->getModelType(),ModelType::StLouis);
-    ASSERT_EQ(saintLouis->getNumPlanes(),0);
-    
-    
-    
-    
-    ASSERT_EQ(saintLouis->getPos().x,1000);
-    ASSERT_EQ(saintLouis->getPos().y,1000);
-    ASSERT_EQ(saintLouis->getAcceleration(),3);
-    ASSERT_EQ(saintLouis->getMaxSpeed(),41);
-    ASSERT_EQ(saintLouis->getHp(),11013);
-    ASSERT_EQ(saintLouis->getArmour(),280);
-    ASSERT_EQ(saintLouis->getNationality(),"Usa");
-    ASSERT_EQ(saintLouis->getNumLCannons(),5);
-    ASSERT_EQ(saintLouis->getNumHCannons(),4);
-    ASSERT_EQ(saintLouis->getNumMCannons(),3);
-    ASSERT_EQ(saintLouis->getNumAntiAircraft(),antiAir);
-    ASSERT_EQ(saintLouis->getWidth(),shipWidth);
-    ASSERT_EQ(saintLouis->getLength(),shipHeight);
-    ASSERT_EQ(saintLouis->getCol(),true);
-    ASSERT_EQ(saintLouis->getShipType(),ShipType::Cruiser);
-    ASSERT_EQ(saintLouis->getModelType(),ModelType::StLouis);
-    ASSERT_EQ(saintLouis->getNumPlanes(),0);
+    ASSERT_EQ(saintLouis->getModelType(),ModelType::Trento);
+    ASSERT_EQ(saintLouis->getNumPlanes(),2);
     
     
     
