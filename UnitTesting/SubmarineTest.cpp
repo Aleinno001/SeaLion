469c3a9dd55558b129a538fa4e2f3a0ab842b861
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-19 03:07:07 -0700

    Aggiunto codice di unit testing per sottomarino I400

--- UnitTesting/SubmarineTest.cpp
+++ UnitTesting/SubmarineTest.cpp
@@ -119,14 +119,24 @@ TEST(Submarine, Constructor) {
     ASSERT_EQ(typeb1->isSubmerged1(),false);
 
 
+    shipWidth = 13;
+    shipHeight = 122;
+
+
+    std::unique_ptr<Submarine> i400(
+            new Submarine(1000,1000, 2, 35, 6670, 0, "Japan", 0, 0, 0, 0,  v, shipHeight, shipWidth,
+                          true,
+                          ShipType::Submarine, ModelType::I400, 8, false));
+
+
 
     ASSERT_EQ(Gato->getPos().x,1000);
     ASSERT_EQ(Gato->getPos().y,1000);
     ASSERT_EQ(Gato->getAcceleration(),2);
-    ASSERT_EQ(Gato->getMaxSpeed(),37);
-    ASSERT_EQ(Gato->getHp(),2460);
+    ASSERT_EQ(Gato->getMaxSpeed(),35);
+    ASSERT_EQ(Gato->getHp(),6670);
     ASSERT_EQ(Gato->getArmour(),0);
-    ASSERT_EQ(Gato->getNationality(),"Usa");
+    ASSERT_EQ(Gato->getNationality(),"Japan");
     ASSERT_EQ(Gato->getNumLCannons(),0);
     ASSERT_EQ(Gato->getNumHCannons(),0);
     ASSERT_EQ(Gato->getNumMCannons(),0);
@@ -135,8 +145,8 @@ TEST(Submarine, Constructor) {
     ASSERT_EQ(Gato->getLength(),shipHeight);
     ASSERT_EQ(Gato->getCol(),true);
     ASSERT_EQ(Gato->getShipType(),ShipType::Submarine);
-    ASSERT_EQ(Gato->getModelType(),ModelType::Gato);
-    ASSERT_EQ(Gato->getNumTorpedoTubes(),6);
+    ASSERT_EQ(Gato->getModelType(),ModelType::I400);
+    ASSERT_EQ(Gato->getNumTorpedoTubes(),8);
     ASSERT_EQ(Gato->isSubmerged1(),false);
 
 
