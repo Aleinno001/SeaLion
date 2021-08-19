c1d5662567821e3a920d42a7d532edc46c42be9f
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-19 03:12:32 -0700

    Aggiunto codice di unit testing per sottomarino Narwhal

--- UnitTesting/SubmarineTest.cpp
+++ UnitTesting/SubmarineTest.cpp
@@ -130,45 +130,56 @@ TEST(Submarine, Constructor) {
 
 
 
-    ASSERT_EQ(Gato->getPos().x,1000);
-    ASSERT_EQ(Gato->getPos().y,1000);
-    ASSERT_EQ(Gato->getAcceleration(),2);
-    ASSERT_EQ(Gato->getMaxSpeed(),35);
-    ASSERT_EQ(Gato->getHp(),6670);
-    ASSERT_EQ(Gato->getArmour(),0);
-    ASSERT_EQ(Gato->getNationality(),"Japan");
-    ASSERT_EQ(Gato->getNumLCannons(),0);
-    ASSERT_EQ(Gato->getNumHCannons(),0);
-    ASSERT_EQ(Gato->getNumMCannons(),0);
-    ASSERT_EQ(Gato->getNumAntiAircraft(),0);
-    ASSERT_EQ(Gato->getWidth(),shipWidth);
-    ASSERT_EQ(Gato->getLength(),shipHeight);
-    ASSERT_EQ(Gato->getCol(),true);
-    ASSERT_EQ(Gato->getShipType(),ShipType::Submarine);
-    ASSERT_EQ(Gato->getModelType(),ModelType::I400);
-    ASSERT_EQ(Gato->getNumTorpedoTubes(),8);
-    ASSERT_EQ(Gato->isSubmerged1(),false);
+    ASSERT_EQ(i400->getPos().x,1000);
+    ASSERT_EQ(i400->getPos().y,1000);
+    ASSERT_EQ(i400->getAcceleration(),2);
+    ASSERT_EQ(i400->getMaxSpeed(),35);
+    ASSERT_EQ(i400->getHp(),6670);
+    ASSERT_EQ(i400->getArmour(),0);
+    ASSERT_EQ(i400->getNationality(),"Japan");
+    ASSERT_EQ(i400->getNumLCannons(),0);
+    ASSERT_EQ(i400->getNumHCannons(),0);
+    ASSERT_EQ(i400->getNumMCannons(),0);
+    ASSERT_EQ(i400->getNumAntiAircraft(),0);
+    ASSERT_EQ(i400->getWidth(),shipWidth);
+    ASSERT_EQ(i400->getLength(),shipHeight);
+    ASSERT_EQ(i400->getCol(),true);
+    ASSERT_EQ(i400->getShipType(),ShipType::Submarine);
+    ASSERT_EQ(i400->getModelType(),ModelType::I400);
+    ASSERT_EQ(i400->getNumTorpedoTubes(),8);
+    ASSERT_EQ(i400->isSubmerged1(),false);
+    
+    
+    shipWidth = 13;
+    shipHeight = 91;
+
+  
+    std::unique_ptr<Submarine> Narwhal(
+            new Submarine(1000,1000, 2, 48, 4600, 0, "Usa", 0, 0, 0, 0,  v, shipHeight, shipWidth, true,
+                          ShipType::Submarine, ModelType::Narwhal, 4, false));
 
 
 
-    ASSERT_EQ(Gato->getPos().x,1000);
-    ASSERT_EQ(Gato->getPos().y,1000);
-    ASSERT_EQ(Gato->getAcceleration(),2);
-    ASSERT_EQ(Gato->getMaxSpeed(),37);
-    ASSERT_EQ(Gato->getHp(),2460);
-    ASSERT_EQ(Gato->getArmour(),0);
-    ASSERT_EQ(Gato->getNationality(),"Usa");
-    ASSERT_EQ(Gato->getNumLCannons(),0);
-    ASSERT_EQ(Gato->getNumHCannons(),0);
-    ASSERT_EQ(Gato->getNumMCannons(),0);
-    ASSERT_EQ(Gato->getNumAntiAircraft(),0);
-    ASSERT_EQ(Gato->getWidth(),shipWidth);
-    ASSERT_EQ(Gato->getLength(),shipHeight);
-    ASSERT_EQ(Gato->getCol(),true);
-    ASSERT_EQ(Gato->getShipType(),ShipType::Submarine);
-    ASSERT_EQ(Gato->getModelType(),ModelType::Gato);
-    ASSERT_EQ(Gato->getNumTorpedoTubes(),6);
-    ASSERT_EQ(Gato->isSubmerged1(),false);
+    ASSERT_EQ(Narwhal->getPos().x,1000);
+    ASSERT_EQ(Narwhal->getPos().y,1000);
+    ASSERT_EQ(Narwhal->getAcceleration(),2);
+    ASSERT_EQ(Narwhal->getMaxSpeed(),48);
+    ASSERT_EQ(Narwhal->getHp(),4600);
+    ASSERT_EQ(Narwhal->getArmour(),0);
+    ASSERT_EQ(Narwhal->getNationality(),"Usa");
+    ASSERT_EQ(Narwhal->getNumLCannons(),0);
+    ASSERT_EQ(Narwhal->getNumHCannons(),0);
+    ASSERT_EQ(Narwhal->getNumMCannons(),0);
+    ASSERT_EQ(Narwhal->getNumAntiAircraft(),0);
+    ASSERT_EQ(Narwhal->getWidth(),shipWidth);
+    ASSERT_EQ(Narwhal->getLength(),shipHeight);
+    ASSERT_EQ(Narwhal->getCol(),true);
+    ASSERT_EQ(Narwhal->getShipType(),ShipType::Submarine);
+    ASSERT_EQ(Narwhal->getModelType(),ModelType::Narwhal);
+    ASSERT_EQ(Narwhal->getNumTorpedoTubes(),6);
+    ASSERT_EQ(Narwhal->isSubmerged1(),false);
+
+
 
 
 
