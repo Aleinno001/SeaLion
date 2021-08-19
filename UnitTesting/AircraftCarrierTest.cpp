6e55edb86a2d134192dbd05feb9b2e6c0c9593bd
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-19 01:14:47 -0700

    Aggiunto codice di unit testing per portaerei giapponesi

--- UnitTesting/AircraftCarrierTest.cpp
+++ UnitTesting/AircraftCarrierTest.cpp
@@ -124,7 +124,137 @@ TEST(AircraftCarrier, Constructor) {
     ASSERT_EQ(Tahio->getNumPlanes(),7);
 
 
+    shipWidth = 70;
+    shipHeight = 295;
 
+
+    numAntiAir = 8;
+
+
+    std::unique_ptr<AircraftCarrier> FranklinDRoosevelt(
+            new AircraftCarrier(1000,1000, 1, 61, 45000, 363, "Usa", 0, 0, 2, numAntiAir,  v,
+                                shipHeight,
+                                shipWidth,
+                                true,
+                                ShipType::AircraftCarrier, ModelType::FranklinDRoosevelt, 14));
+
+    ASSERT_EQ(FranklinDRoosevelt->getPos().x,1000);
+    ASSERT_EQ(FranklinDRoosevelt->getPos().y,1000);
+    ASSERT_EQ(FranklinDRoosevelt->getAcceleration(),1);
+    ASSERT_EQ(FranklinDRoosevelt->getMaxSpeed(),61);
+    ASSERT_EQ(FranklinDRoosevelt->getHp(),45000);
+    ASSERT_EQ(FranklinDRoosevelt->getArmour(),363);
+    ASSERT_EQ(FranklinDRoosevelt->getNationality(),"Usa");
+    ASSERT_EQ(FranklinDRoosevelt->getNumLCannons(),0);
+    ASSERT_EQ(FranklinDRoosevelt->getNumHCannons(),0);
+    ASSERT_EQ(FranklinDRoosevelt->getNumMCannons(),2);
+    ASSERT_EQ(FranklinDRoosevelt->getNumAntiAircraft(),numAntiAir);
+    ASSERT_EQ(FranklinDRoosevelt->getWidth(),shipWidth);
+    ASSERT_EQ(FranklinDRoosevelt->getLength(),shipHeight);
+    ASSERT_EQ(FranklinDRoosevelt->getCol(),true);
+    ASSERT_EQ(FranklinDRoosevelt->getShipType(),ShipType::AircraftCarrier);
+    ASSERT_EQ(FranklinDRoosevelt->getModelType(),ModelType::FranklinDRoosevelt);
+    ASSERT_EQ(FranklinDRoosevelt->getNumPlanes(),14);
+    
+    shipWidth = 41;
+    shipHeight = 230;
+
+    numAntiAir = 6;
+
+    std::unique_ptr<AircraftCarrier> Indomitable(
+            new AircraftCarrier(1000,1000, 1, 56, 29730, 240, "Uk", 2, 0, 0, numAntiAir,  v,
+                                shipHeight,
+                                shipWidth,
+                                true,
+                                ShipType::AircraftCarrier, ModelType::Indomitable, 5));
+    
+    
+    ASSERT_EQ(Indomitable->getPos().x,1000);
+    ASSERT_EQ(Indomitable->getPos().y,1000);
+    ASSERT_EQ(Indomitable->getAcceleration(),1);
+    ASSERT_EQ(Indomitable->getMaxSpeed(),56);
+    ASSERT_EQ(Indomitable->getHp(),29730);
+    ASSERT_EQ(Indomitable->getArmour(),240);
+    ASSERT_EQ(Indomitable->getNationality(),"Uk");
+    ASSERT_EQ(Indomitable->getNumLCannons(),2);
+    ASSERT_EQ(Indomitable->getNumHCannons(),0);
+    ASSERT_EQ(Indomitable->getNumMCannons(),0);
+    ASSERT_EQ(Indomitable->getNumAntiAircraft(),numAntiAir);
+    ASSERT_EQ(Indomitable->getWidth(),shipWidth);
+    ASSERT_EQ(Indomitable->getLength(),shipHeight);
+    ASSERT_EQ(Indomitable->getCol(),true);
+    ASSERT_EQ(Indomitable->getShipType(),ShipType::AircraftCarrier);
+    ASSERT_EQ(Indomitable->getModelType(),ModelType::Indomitable);
+    ASSERT_EQ(Indomitable->getNumPlanes(),5);
+    
+    
+    shipWidth = 50;
+    shipHeight = 244;
+
+    
+
+    numAntiAir = 3;
+
+
+    std::unique_ptr<AircraftCarrier> Cavour(
+            new AircraftCarrier(1000, 1000, 1, 52, 29900, 241, "Italy", 4, 0, 0, numAntiAir,  v,
+                                shipHeight,
+                                shipWidth,
+                                true,
+                                ShipType::AircraftCarrier, ModelType::Cavour, 5));
+    
+    ASSERT_EQ(Cavour->getPos().x,1000);
+    ASSERT_EQ(Cavour->getPos().y,1000);
+    ASSERT_EQ(Cavour->getAcceleration(),1);
+    ASSERT_EQ(Cavour->getMaxSpeed(),52);
+    ASSERT_EQ(Cavour->getHp(),29900);
+    ASSERT_EQ(Cavour->getArmour(),241);
+    ASSERT_EQ(Cavour->getNationality(),"Italy");
+    ASSERT_EQ(Cavour->getNumLCannons(),4);
+    ASSERT_EQ(Cavour->getNumHCannons(),0);
+    ASSERT_EQ(Cavour->getNumMCannons(),0);
+    ASSERT_EQ(Cavour->getNumAntiAircraft(),numAntiAir);
+    ASSERT_EQ(Cavour->getWidth(),shipWidth);
+    ASSERT_EQ(Cavour->getLength(),shipHeight);
+    ASSERT_EQ(Cavour->getCol(),true);
+    ASSERT_EQ(Cavour->getShipType(),ShipType::AircraftCarrier);
+    ASSERT_EQ(Cavour->getModelType(),ModelType::Cavour);
+    ASSERT_EQ(Cavour->getNumPlanes(),5);
+
+
+    shipWidth = 39;
+    shipHeight = 222;
+
+
+
+   numAntiAir = 2;
+
+
+    std::unique_ptr<AircraftCarrier> Hiryu(
+            new AircraftCarrier(1000, 1000, 1, 63, 20570, 70, "Japan", 2, 0, 0, numAntiAir,  v,
+                                shipHeight,
+                                shipWidth,
+                                true,
+                                ShipType::AircraftCarrier, ModelType::Hiryu, 6));
+
+
+    ASSERT_EQ(mid->getPos().x,1000);
+    ASSERT_EQ(mid->getPos().y,1000);
+    ASSERT_EQ(mid->getAcceleration(),1);
+    ASSERT_EQ(mid->getMaxSpeed(),63);
+    ASSERT_EQ(mid->getHp(),20570);
+    ASSERT_EQ(mid->getArmour(),70);
+    ASSERT_EQ(mid->getNationality(),"Japan");
+    ASSERT_EQ(mid->getNumLCannons(),2);
+    ASSERT_EQ(mid->getNumHCannons(),0);
+    ASSERT_EQ(mid->getNumMCannons(),0);
+    ASSERT_EQ(mid->getNumAntiAircraft(),numAntiAir);
+    ASSERT_EQ(mid->getWidth(),shipWidth);
+    ASSERT_EQ(mid->getLength(),shipHeight);
+    ASSERT_EQ(mid->getCol(),true);
+    ASSERT_EQ(mid->getShipType(),ShipType::AircraftCarrier);
+    ASSERT_EQ(mid->getModelType(),ModelType::Hiryu);
+    ASSERT_EQ(mid->getNumPlanes(),6);
 
 
 
