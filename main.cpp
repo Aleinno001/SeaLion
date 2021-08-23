3be845e904152a1bc9a478cc2c8bc36a262b16f1
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-23 04:08:10 -0700

    Spostata la creazione del thread dalla update a prima che inizi il game loop;

--- main.cpp
+++ main.cpp
@@ -144,7 +144,7 @@ void collisonControl(std::list<iteratorPositions> &fullNavyCollision)//Scorre la
 
 
 
-void update( std::list<iteratorPositions> &lst, double dt,std::list<iteratorPositions> &fullNavyCollision){
+void update( std::list<iteratorPositions> &lst, double dt){
     if(!lst.empty()){
         for (auto iter = lst.begin(); iter != lst.end();) {
             if((iter->it->get()->getSprite().getPosition().x)==iter->pos.x && iter->it->get()->getSprite().getPosition().y==iter->pos.y){
@@ -158,7 +158,8 @@ void update( std::list<iteratorPositions> &lst, double dt,std::list<iteratorPosi
         }
     }
 
-    std::thread thread_collision(f,std::ref(fullNavyCollision));
+
+
 
 
 
@@ -213,6 +214,8 @@ int main() {
         itPos.it = itEnemy;
         fullNavyCollision.push_back(itPos);
     }
+    std::thread thread_collision(f,std::ref(fullNavyCollision)); //Tread separato che verifica costantemente se avvengono collisioni tra tutte le navi di gioco
+    thread_collision.detach();
     while (window.isOpen()) {
         sf::Event event;
 
@@ -342,7 +345,7 @@ int main() {
 
         }
 
-        update(lst,clock.restart().asSeconds(),fullNavyCollision);
+        update(lst,clock.restart().asSeconds());
 
         window.display();
     }
