d112c8fdc76bc2b01c89d1365320d845c8cc7e98
Gabriele <gabriele.santanni@stud.unifi.it>
2021-08-31 02:46:04 -0700

    Aggiunta Factory TorpedoBomber

--- AirplaneFactory.cpp
+++ AirplaneFactory.cpp
@@ -9,14 +9,14 @@
 
 std::unique_ptr<Vehicle> AirplaneFactory::createBomber(int posX, int posY,WarShip &subject) {
 
-    std::unique_ptr<Bomber> bomber(new Bomber(posX,posY,3,60,500,26,35,true,300,900,"Bomber",subject));
+    std::unique_ptr<Bomber> bomber(new Bomber(posX,posY,3,60,500,26,35,true,150,900,"Bomber",subject));
     return std::move(bomber);
 
 }
 
 std::unique_ptr<Vehicle> AirplaneFactory::createTorpedoBomber(int posX, int posY,WarShip &subject) {
 
-    std::unique_ptr<TorpedoBomber> torpedoBomber(new Bomber(posX,posY,3,60,500,26,35,true,300,900,"Bomber",subject));
+    std::unique_ptr<TorpedoBomber> torpedoBomber(new TorpedoBomber(posX,posY,4,70,400,35,35,true,20,1500,"TorpedoBomber",subject));
     return std::move(torpedoBomber);
 }
 
