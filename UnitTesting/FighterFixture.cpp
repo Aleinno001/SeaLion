489089e65cba9bb800978aba4139cfbc06cd1a75
Gabriele <gabriele.santanni@stud.unifi.it>
2021-09-02 03:25:34 -0700

    Aggiunto codice di unit testing per i fighter

---
+++ UnitTesting/FighterFixture.cpp
@@ -0,0 +1,19 @@
+//
+// Created by pala on 9/2/21.
+//
+#include "gtest/gtest.h"
+#include "../Fighter.h"
+
+
+class LightCannonSuite : public ::testing::Test {
+protected:
+
+protected:
+    virtual void SetUp() {
+
+
+
+    }
+
+    Fighter c;
+};
