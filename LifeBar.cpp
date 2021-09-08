3da2411e2d66e5581b5b22a6e4b3adede9601c8f
Gabriele <gabriele.santanni@stud.unifi.it>
2021-09-08 02:57:57 -0700

    Aggiornata dinamica cambioo colore barre di stato

--- LifeBar.cpp
+++ LifeBar.cpp
@@ -9,7 +9,7 @@
 
 
 
-LifeBar::LifeBar(WarShip &subject):BarInterface(),subject_(subject),r(15),g(135),b(7) {
+LifeBar::LifeBar(WarShip &subject):BarInterface(),subject_(subject),r(0),g(255),b(0) {
 
     life.setPosition(subject_.getSprite().getPosition().x+0.50*subject_.getWidth(),subject_.getSprite().getPosition().y+0.50*subject_.getLength());
     life.setColor(sf::Color(r,g,b));
@@ -34,10 +34,12 @@ void LifeBar::updateBarsDamage() {
     double percentageLife = (subject_.getHp() * subject_.getMaxHp()) / 100;
     double damageReceived = abs(subject_.getMaxHp() - subject_.getHp());
     double percentageDamage = (damageReceived * subject_.getMaxHp()) / 100;
-    double fifty = ((subject_.getMaxHp() / 2) * subject_.getMaxHp()) / 100;
-    if (percentageLife > fifty) {
-        r = r + r * percentageDamage;
-        life.setColor(sf::Color(r, g, b));
+
+    if (percentageLife > 0.50) {
+        if(r<255) {
+            r = r + r * percentageDamage;
+            life.setColor(sf::Color(r, g, b));
+        }
     } else {
         g = g - g * percentageDamage;
         life.setColor(sf::Color(r, g, b));
