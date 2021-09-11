f99ac839787960ab3276946f36aa4976e574b31b
Gabriele <gabriele.santanni@stud.unifi.it>
2021-09-11 01:57:23 -0700

    Creata classe interfaccia per implementare gli observer per design pattern MVC

---
+++ MvcObserver.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pala on 9/11/21.
+//
+
+#include "MvcObserver.h"
---
+++ MvcObserver.h
@@ -0,0 +1,14 @@
+//
+// Created by pala on 9/11/21.
+//
+
+#ifndef SEALION_MVCOBSERVER_H
+#define SEALION_MVCOBSERVER_H
+
+
+class MvcObserver {
+
+};
+
+
+#endif //SEALION_MVCOBSERVER_H
