f4ba6e18b3dbc2390ad37f91eea1b256df96ed2b
Gabriele <gabriele.santanni@stud.unifi.it>
2021-09-11 02:15:12 -0700

    Aggiunta Classe MvcController per designb pattern MVC

---
+++ MvcController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pala on 9/11/21.
+//
+
+#include "MvcController.h"
---
+++ MvcController.h
@@ -0,0 +1,14 @@
+//
+// Created by pala on 9/11/21.
+//
+
+#ifndef SEALION_MVCCONTROLLER_H
+#define SEALION_MVCCONTROLLER_H
+
+
+class MvcController {
+
+};
+
+
+#endif //SEALION_MVCCONTROLLER_H
